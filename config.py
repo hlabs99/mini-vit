79264cbafb7393c95fe5a4e3114ce89657778bb2
Daniel Michael Berhane <berhane.daniel1729@gmail.com>
2026-06-29 00:25:13 +0200

    Add configuration file containing all necessary hyper parameters

---
+++ config.py
@@ -0,0 +1,26 @@
+
+# Configuration file containing all hyperparameters and setting for model training and evaluation
+
+# Data parameters
+BATCH_SIZE = 64
+IMG_SIZE = 32                                    # Image dimensions
+NUM_CLASSES = 10
+
+# Patch embedding parameters
+PATCH_SIZE = 4
+NUM_PATCHES = (IMG_SIZE // PATCH_SIZE) ** 2     # Number of patches depending on image and patch size
+PATCH_DIM = PATCH_SIZE * PATCH_SIZE * 3         # flattened patch size
+
+
+# Transformer model parmeters
+D_MODEL = 128                                   # Embedding dimension
+NUM_HEADS = 4                                   # Number of attention heads
+NUM_LAYERS = 6
+MLP_DIM = 256
+DROPOUT = 0.1
+
+
+# Training parameters
+LR = 1e-3 
+NUM_EPOCHS = 10
+
