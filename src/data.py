41eace74fb2c5ee1866af08f16d6ac4c625a1c12
Daniel Michael Berhane <berhane.daniel1729@gmail.com>
2026-06-29 08:46:49 +0200

    Fixed a typo (transform --> transforms)

--- src/data.py
+++ src/data.py
@@ -4,13 +4,13 @@ from torch.utils.data import DataLoader
 from config import BATCH_SIZE
 
 def get_cifar10_dataloaders():
-    transform = transform.Compose([
+    transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
-    train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    test_dataset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    train_dataset = datasets.CIFAR10(root='./dataset', train=True, download=True, transform=transform)
+    test_dataset = datasets.CIFAR10(root='./dataset', train=False, download=True, transform=transform)
 
     train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
     test_loader = DataLoader(test_dataset, batch_size=BATCH_SIZE, shuffle=False)
